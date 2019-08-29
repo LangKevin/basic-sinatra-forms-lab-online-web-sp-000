05e9282593af028e7dffa4ade46a4fb29d7d3b06
kevin lang <langkevina@gmail.com>
2019-08-29 01:24:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
-  post '/food' do
+  post '/team' do
 # params.to_s
 "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
