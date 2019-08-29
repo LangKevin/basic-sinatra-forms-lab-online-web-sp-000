989d804782233574131180424fa18a7dfd196624
kevin lang <langkevina@gmail.com>
2019-08-29 01:14:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
-require 'sinatra/base'
-
 class App < Sinatra::Base
-
+  get '/' do
+     "Hello World"
+   end
+  get '/food_form' do
+    erb :food_form
+  end
+  post '/food' do
+# params.to_s
+"My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+  # Add your post route and action below
 
 end
